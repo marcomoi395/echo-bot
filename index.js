295eb3791d39c6a748b0e9afeb019d1179eb7828
youngmarco <marcomoi395@gmail.com>
2024-10-30 14:22:26 +0700

    add: syncBank command

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const {Telegraf, session} = require('telegraf')
 const middleware = require('./middleware/auth.middleware')
 const textHandlers = require('./handler/textHandlers')
 const scheduleDailyMessage = require("./handler/scheduleDailyMessage");
+const axios = require('axios');
 const {connect} = require("./config/database");
 require('dotenv').config()
 
@@ -9,6 +10,16 @@ global.bot = new Telegraf(process.env.BOT_TOKEN)
 bot.use(session());
 
 connect();
+
+const syncData = async () => {
+    try {
+        // Thay đổi URL dưới đây thành đường dẫn bạn muốn truy cập
+        const response = await axios.get('http://app:3000/gateways/start-gate?name=mb_bank_1&time_in_sec=60');
+        console.log('Đồng bộ hóa thành công:', response.data);
+    } catch (error) {
+        console.error('Lỗi khi đồng bộ hóa:', error);
+    }
+};
 
 // Schedule Daily Message
 scheduleDailyMessage(bot);
@@ -19,6 +30,12 @@ bot.start((ctx) => ctx.reply('Welcome'))
 bot.command("recommend", middleware.auth, textHandlers.sendLastestRequest);
 
 bot.command("confession", middleware.auth, textHandlers.addConfession);
+
+bot.command('syncBank', (ctx) => {
+    ctx.reply('Đang thực hiện đồng bộ hóa...');
+    syncData();
+    ctx.reply('Đã hoàn tất đồng bộ hóa!');
+})
 
 // Message handling
 bot.on("message", middleware.auth, textHandlers.message);
